066a9d980b76470d71fe897ee610333cba3f1fe4
Mayur <99334268+devopsmayur@users.noreply.github.com>
2025-12-29 00:10:26 +0530

    Change machine type from 't3.medium' to 't3.medium2'

--- vm_creation.tf
+++ vm_creation.tf
@@ -18,7 +18,7 @@ output "network_info" {
 # Create a virtual machine instance
 resource "google_compute_instance" "world11" {
   name         = "my-instance7"
-  machine_type = "t3.medium"
+  machine_type = "t3.medium2"
   zone         = "us-central1-a"
 
   boot_disk {
